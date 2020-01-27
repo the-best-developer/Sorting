a859be8102a2dc77faf8c0edcce08948b8943f66
the-best-developer <im.dylan.gosnell@gmail.com>
2020-01-26 22:44:04 -0500

    added an insertion sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,6 +1,6 @@
 # TO-DO: Complete the selection_sort() function below 
 
-test_array = [ 5, 4, 7, 1, 8, 3, 5, 2, 19, 34, 12 ]
+test_array = [ 4, 2, 3, 1, 9, 8, 7, 6, 5 ]
 
 ##################################################
 # 
@@ -34,7 +34,7 @@ test_array = [ 5, 4, 7, 1, 8, 3, 5, 2, 19, 34, 12 ]
 def selection_sort( arr ):
     # loop through n-1 elements
 
-    for i in range(0, len(arr)):
+    for i in range(0, len(arr) - 1):
 
         # Define smallest index as i because you do not check numbers before i in the list
         smallest_index = i
@@ -62,6 +62,25 @@ def selection_sort( arr ):
 
     return arr
 
+def insertion_sort( arr ):
+
+    # Loop through array
+    for i in range(1, len(arr)):
+        
+        # loop through index 0 to i
+        for j in range(0, i):
+
+            # we assume our numbers are in order from 0 to i
+            # we can then assume the first number that is larger then what is selected is where we should insert our number
+            if arr[j] > arr[i]:
+                value = arr.pop(i)
+                arr.insert(j, value)
+                print(f"put: {value} at: {j}")
+                print(arr)
+                break
+
+    return arr
+
 
 
 # TO-DO:  implement the Bubble Sort function below
@@ -77,4 +96,4 @@ def count_sort( arr, maximum=-1 ):
 
 print(test_array)
 # print(new_array_sort(test_array))
-selection_sort(test_array)
+print(insertion_sort(test_array))
